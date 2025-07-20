482c12d0713d40a10ddc88d74931dc1a73e14fa2
victor aragão <victoraragao@gmail.com>
2025-07-19 22:54:23 -0300

    divider my-6 to my-2

--- src/components/keypad/KeypadDivider.tsx
+++ src/components/keypad/KeypadDivider.tsx
@@ -1,6 +1,6 @@
 export const KeypadDivider = () => {
   return (
-    <div className="w-full my-6">
+    <div className="w-full my-2">
       <div className="border bg-[#1E09BB] z-10 shrink-0 h-px border-[rgba(30,9,187,1)] border-solid" />
     </div>
   );
